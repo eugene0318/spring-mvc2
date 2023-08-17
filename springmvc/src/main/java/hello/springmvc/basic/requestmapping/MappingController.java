de6e4f2307f72eb4d48f7668820338f5eeca068f
eugene0318 <nttott@gmail.com>
2023-08-18 01:20:42 +0900

    ~mapping

--- springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
+++ springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingController.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -60,6 +61,37 @@ public class MappingController {
 	@GetMapping(value = "/mapping-param", params = "mode=debug")
 	public String mappingParam() {
 		log.info("mappingParam");
+		return "ok";
+	}
+
+	/**
+	 * 특정 헤더로 추가 매핑 headers="mode", headers="!mode" headers="mode=debug"
+	 * headers="mode!=debug" (! = )
+	 */
+	@GetMapping(value = "/mapping-header", headers = "mode=debug")
+	public String mappingHeader() {
+		log.info("mappingHeader");
+		return "ok";
+	}
+
+	/**
+	 * Content-Type 헤더 기반 추가 매핑 Media Type consumes="application/json"
+	 * consumes="!application/json" consumes="application/*" consumes="*\/*"
+	 * MediaType.APPLICATION_JSON_VALUE
+	 */
+	@PostMapping(value = "/mapping-consume", consumes = "application/json")
+	public String mappingConsumes() {
+		log.info("mappingConsumes");
+		return "ok";
+	}
+
+	/**
+	 * Accept 헤더 기반 Media Type produces = "text/html" produces = "!text/html"
+	 * produces = "text/*" produces = "*\/*"
+	 */
+	@PostMapping(value = "/mapping-produce", produces = "text/html")
+	public String mappingProduces() {
+		log.info("mappingProduces");
 		return "ok";
 	}
 }
